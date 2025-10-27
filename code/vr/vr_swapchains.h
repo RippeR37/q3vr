b1b0fa8d4b3546618cfab116395d870a64c9a567
Ernie Miller <ernie@ernie.io>
2025-10-26 20:13:12 -0400

    Further fixes for 1st person following
    Should really get around to just making the vr->virtual_screen flag the
    one source of truth, maybe? Or maybe not. Anyway, didn't like how the
    blitting of the full VR view to a 4:3 aspect ratio squished things, so
    did entirely too much work to make that not happen.

--- code/vr/vr_swapchains.h
+++ code/vr/vr_swapchains.h
@@ -1,6 +1,7 @@
 #ifndef __VR_SWAPCHAINS
 #define __VR_SWAPCHAINS
 
+#include "../qcommon/q_shared.h"
 #include "vr_types.h"
 
 typedef enum
@@ -17,7 +18,7 @@ void VR_DestroySwapchains(VR_SwapchainInfos* swapchains);
 
 void VR_Swapchains_BindFramebuffers(VR_SwapchainInfos* swapchains, uint32_t swapchainColorIndex, uint32_t swapchainDepthIndex);
 void VR_Swapchains_BlitXRToMainFbo(VR_SwapchainInfos* swapchains, uint32_t swapchainImageIndex, XrDesktopViewConfiguration viewConfig);
-void VR_Swapchains_BlitXRToVirtualScreen(VR_SwapchainInfos* swapchains, uint32_t swapchainImageIndex);
+void VR_Swapchains_BlitXRToVirtualScreen(VR_SwapchainInfos* swapchains, uint32_t swapchainImageIndex, qboolean use4x3Crop);
 
 void VR_Swapchains_Acquire(VR_SwapchainInfos* swapchains, uint32_t* colorIndex, uint32_t* depthIndex);
 void VR_Swapchains_Release(VR_SwapchainInfos* swapchains);
